c4ecf3ab4e535c16c146f7fa9e5f06d2f0aa3b4a
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-09-18 01:31:23 +0300

    RailFence encrypt/decrypt

--- RailFence.php
+++ RailFence.php
@@ -5,8 +5,92 @@ class RailFence implements Cipher
 {
     public function decrypt($message)
     {
-        // TODO: Implement decrypt() method.
+        $key = (int)explode('-', $message)[0];
+        $message = str_split(explode('-', $message)[1]);
+
+        //Make Rail Matrix
+        $bigArray = [];
+        for($i = 0; $i <= $key; $i++)
+        {
+            for($j = 0, $jMax = count($message) ; $j < $jMax; $j++)
+            {
+                $bigArray[$i][$j] = "\n";
+            }
+        }
+
+        $dirDown = null;
+        $row = 0;
+        $col = 0;
+
+        for($i = 0, $iMax = count($message); $i < $iMax; $i++)
+        {
+            if($row === 0)
+            {
+                $dirDown = true;
+            }
+            if($row === $key - 1)
+            {
+                $dirDown = false;
+            }
+
+            $bigArray[$row][$col] = "*";
+            ++$col;
+
+            if($dirDown)
+            {
+                ++$row;
+            } else {
+                --$row;
+            }
+
+        }
+
+        $index = 0;
+        for($i = 0; $i < $key; $i++)
+        {
+            for($j = 0, $jMax = count($message) ; $j < $jMax; $j++ )
+            {
+                if(($bigArray[$i][$j] === "*") && ($index < count($message)))
+                {
+                    $bigArray[$i][$j] = $message[$index];
+                    ++$index;
+                }
+            }
+        }
+
+        $result = '';
+        $row = 0;
+        $col = 0;
+
+        for($i = 0, $iMax = count($message); $i < $iMax; $i++)
+        {
+            if($row === 0)
+            {
+                $dirDown = true;
+            }
+            if($row === $key - 1)
+            {
+                $dirDown = false;
+            }
+
+            if($bigArray[$row][$col] !== "*")
+            {
+                $result .= $bigArray[$row][$col];
+                ++$col;
+            }
+
+            if($dirDown)
+            {
+                ++$row;
+            } else {
+                --$row;
+            }
+        }
+
+        echo $result;
     }
+
+
 
     public function encrypt($message)
     {

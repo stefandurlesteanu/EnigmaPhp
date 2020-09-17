e8b03461ef50857d1db9bd4ab2ce463de2bb7e90
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-09-17 23:48:22 +0300

    RailFence encrypt

--- RailFence.php
+++ RailFence.php
@@ -10,7 +10,35 @@ class RailFence implements Cipher
 
     public function encrypt($message)
     {
-        // TODO: Implement encrypt() method.
-    }
+        $pivot = 0;
+        $key = (int)explode('-', $message)[0]-1;
+        $message = str_split(explode('-', $message)[1]);
+        $encryptedMessage = '';
+        $bigArray = null;
 
+        for ($i = 0; $i <= $key; $i++) {
+            $bigArray[] = array();
+        }
+
+        while (count($message) > 0) {
+            if ($pivot === 0) {
+                for ($i = 0; $i < $key; $i++) {
+                    $var = (array_shift($message));
+                    $bigArray[$i][] = $var;
+                    $pivot++;
+                }
+            }
+
+            if ($pivot === $key) {
+                for ($i = $pivot; $i >= 1; $i--) {
+                    $var = (array_shift($message));
+                    $bigArray[$i][] = $var;
+                    --$pivot;
+                }
+            }
+        }
+
+        $bigArray = array_merge(...array_values($bigArray));
+        echo implode($bigArray);
+    }
 }

2ae129bdf1f6b7fbb1dd4b94d3065c8b92816576
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-09-17 19:15:28 +0300

    Morse encrypt/decrypt

--- MorseCode.php
+++ MorseCode.php
@@ -3,15 +3,69 @@ include 'Cipher.php';
 
 class MorseCode implements Cipher
 {
+    public static $morseCode = array(
+        "A" => ".-",
+        "B" => "-...",
+        "C" => "-.-.",
+        "D" => "-..",
+        "E" => ".",
+        "F" => "..-.",
+        "G" => "--.",
+        "H" => "....",
+        "I" => "..",
+        "J" => ".---",
+        "K" => "-.-",
+        "L" => ".-..",
+        "M" => "--",
+        "N" => "-.",
+        "O" => "---",
+        "P" => ".--.",
+        "Q" => "--.-",
+        "R" => ".-.",
+        "S" => "...",
+        "T" => "-",
+        "U" => "..-",
+        "V" => "...-",
+        "W" => ".--",
+        "X" => "-..-",
+        "Y" => "-.--",
+        "Z" => "--..",
+        "1" => ".----",
+        "2" => "..---",
+        "3" => "...--",
+        "4" => "....-",
+        "5" => ".....",
+        "6" => "-....",
+        "7" => "--...",
+        "8" => "---..",
+        "9" => "----.",
+        "0" => "-----",
+        " " => "/"
+    );
+
     public function decrypt($message)
     {
-        // TODO: Implement decrypt() method.
-        echo $message;
+
+        $decryptedMessage = '';
+        $message = substr($message, 1);
+        $message = explode(" ", $message);
+        foreach ($message as $word){
+            $decryptedMessage .= array_search($word, self::$morseCode, false);
+        }
+        echo $decryptedMessage;
     }
 
     public function encrypt($message)
     {
-        // TODO: Implement encrypt() method.
+        $message = strtoupper($message);
+        $encryptedMessage = '';
+        $messageLength = strlen($message);
+        for ($i = 0; $i < $messageLength; $i++){
+            if(array_key_exists($message[$i], self::$morseCode)){
+                $encryptedMessage .= self::$morseCode[$message[$i]] . " ";
+            }
+        }
+        echo $encryptedMessage;
     }
 
 }
